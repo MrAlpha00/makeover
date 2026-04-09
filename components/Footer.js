b10045ddaa0a971bf018b9c30951783412a120c6
MrAlpha00 <sm4686771@gmail.com>
2026-04-09 23:28:59 +0530

    dkhd

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { MapPin, Phone, Mail, Instagram, Facebook, Youtube } from 'lucide-react';
 
 export default function Footer() {
